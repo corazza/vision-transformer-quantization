1df39621a6b02fe23a010a578f26d12ab5b99767
Jan Corazza <corazzajan@gmail.com>
2022-09-16 17:23:01 +0000

    scripts, some Dockerfile changes

--- scripts/interactive_start.sh
+++ scripts/interactive_start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --gpus all --shm-size=8g --volume ~:/ec2-user -w /ec2-user mmclassification2
+docker run -it -p 0.0.0.0:6007:6007 --gpus all --shm-size=8g --volume ~:/ec2-user -w /ec2-user mmclassification
